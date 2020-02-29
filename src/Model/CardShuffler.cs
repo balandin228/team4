8d0695f1039f6c005e6b967038cba876c1e29fe9
HenrickBolender <40788127+HenrickBolender@users.noreply.github.com>
2020-02-29 14:25:37 +0500

    added cardshuffler

---
+++ src/Model/CardShuffler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace thegame.Model
+{
+    public class CardShuffler : IShuffler<Card>
+    {
+        private Random rnd;
+        public CardShuffler()
+        {
+            rnd = new Random();
+        }
+
+        public IEnumerable<T> Shuffle<T>(IEnumerable<T> sequence)
+        {
+            return sequence
+                .Select(obj => new {Number = rnd.Next(), Obj = obj})
+                .OrderBy(x => x.Number)
+                .Select(x => x.Obj);
+        }
+    }
+}
